6bd8a6a0325d8ca5d7a7565fa31a1fe4c05b1f91
Haozhang Yuan <haozyuan@iu.edu>
2019-10-19 17:59:28 -0400

    Update node(1).js

--- node(1).js
+++ node(1).js
@@ -1,12 +1,12 @@
 var mysql  = require('mysql'); 
   
- var connection = mysql.createConnection({    
+/* var connection = mysql.createConnection({    
    host     : 'localhost',      
    user     : 'wcztzd428p6b',             
    password : 'jobsnuSE12.',      
    port: '3306',                  
    database: 'test1'
- });
+ });*/
 
 /*var connection = mysql.createConnection({    
    host     : 'localhost',      
@@ -15,7 +15,13 @@ var mysql  = require('mysql');
    database: 'new_schema1',
    timeout: 600000
  });*/
-
+ var connection = mysql.createConnection({    
+   host     : 'db.soic.indiana.edu',      
+   user     : 'p565f19_sshriva',             
+   password : 'ShubhangiP565F19',      
+   port: '3306',                  
+   database: 'p565f19_sshriva'
+ });
 connection.connect();
  
 var express = require('express');
@@ -60,22 +66,23 @@ app.get('/register.html', function (req, res) {
    res.sendFile( __dirname + "/" + "register.html" ); 
 })
  
-var  addSql = 'INSERT INTO user_profile(email, password, first_name, last_name, dob, gender, primary_contact, secondary_contact, registration) VALUES(?,?,?,?,?,?,?,?,?)';
+var  addSql = 'INSERT INTO user_profile(email, first_name, last_name, dob, gender, primary_contact, secondary_contact, registration_date,	is_recruiter) VALUES(?,?,?,?,?,?,?,?,?)';
  
 app.post('/process_get', function (req, res) {
   
    var response = {
        "email":req.query.email,
-       "password":req.query.password,
        "first_name":req.query.first_name,
        "last_name":req.query.last_name,
        "dob":req.query.dob,
        "gender":req.query.gender,
        "primary_contact":req.query.primary_contact,
        "secondary_contact":req.query.secondary_contact,
-       "registration":req.query.registration
+       "registration":req.query.registration_date,
+       "is_recruiter":req.query.is_recruiter
+       
    };
-   var  addSqlParams = [req.query.emailid,req.query.password,req.query.first_name,req.query.last_name,req.query.dob,req.query.gender,req.query.primary_contact,req.query.secondary_contact,req.query.registration];
+   var  addSqlParams = [req.query.emailid,req.query.first_name,req.query.last_name,req.query.dob,req.query.gender,req.query.primary_contact,req.query.secondary_contact,req.query.registration,req.query.is_recruiter];
    connection.query(addSql,addSqlParams,function (err, result) {
       if(err){
          console.log('[INSERT ERROR] - ',err.message);
@@ -86,14 +93,14 @@ app.post('/process_get', function (req, res) {
         console.log("OK");     
 });
    console.log(response);
-   //res.end(JSON.stringify(response));
+   res.end(JSON.stringify(response));
 })
 
-app.get('/employee.html', function (req, res) {
+app.get('/employer.html', function (req, res) {
    res.sendFile( __dirname + "/" + "employee.html" ); 
 })
  
-var  addSql = 'INSERT INTO user_profile(employer_name, establishment_date, size, headquarters, website, domain, industry, email_id, user_name) VALUES(?,?,?,?,?,?,?,?,?)';
+var  addSql = 'INSERT INTO user_profile(employer_name, establishment_date, size, headquarters, website, domain, industry, email_id, user_name, registration_date) VALUES(?,?,?,?,?,?,?,?,?)';
  
 app.post('/employee', function (req, res) {
   
@@ -106,9 +113,10 @@ app.post('/employee', function (req, res) {
        "domain":req.query.domain,
        "industry":req.query.industry,
        "email_id":req.query.email_id,
-       "user_name":req.query.user_name
+       "user_name":req.query.user_name,
+       "registration_date":req.query.registration_date 
    };
-   var  addSqlParams = [req.query.employer_name,req.query.establishment_date,req.query.size,req.query.headquarters,req.query.website,req.query.domain,req.query.industry,req.query.email_id,req.query.user_name];
+   var  addSqlParams = [req.query.employer_name,req.query.establishment_date,req.query.size,req.query.headquarters,req.query.website,req.query.domain,req.query.industry,req.query.email_id,req.query.user_name,req.query.registration_date];
    connection.query(addSql,addSqlParams,function (err, result) {
       if(err){
          console.log('[INSERT ERROR] - ',err.message);
@@ -119,14 +127,14 @@ app.post('/employee', function (req, res) {
         console.log("OK");     
 });
    console.log(response);
-   //res.end(JSON.stringify(response));
+   res.end(JSON.stringify(response));
 })
 
 app.get('/job_post.html', function (req, res) {
    res.sendFile( __dirname + "/" + "job_post.html" ); 
 })
  
-var  addSql = 'INSERT INTO user_profile(employer_name, establishment_date, size, headquarters, website, domain, industry, email_id, user_name) VALUES(?,?,?,?,?,?,?,?,?)';
+var  addSql = 'INSERT INTO user_profile(posted_by_id, location, domain, industry, function, description, company_id, job_type, is_active) VALUES(?,?,?,?,?,?,?,?,?)';
  
 app.post('/job_post', function (req, res) {
   
@@ -139,9 +147,9 @@ app.post('/job_post', function (req, res) {
        "description":req.query.description,
        "company_id":req.query.company_id,
        "job_type":req.query.job_type,
-       "is_active":req.query.user_name
+       "is_active":req.query.is_active
    };
-   var  addSqlParams = [req.query.employer_name,req.query.establishment_date,req.query.size,req.query.headquarters,req.query.website,req.query.domain,req.query.industry,req.query.email_id,req.query.user_name];
+   var  addSqlParams = [req.query.posted_by_id,req.query.location,req.query.domain,req.query.industry,req.query.function,req.query.description,-req.query.company_id,req.query.industry,req.query.job_type,req.query.is_active];
    connection.query(addSql,addSqlParams,function (err, result) {
       if(err){
          console.log('[INSERT ERROR] - ',err.message);
